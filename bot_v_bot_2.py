02f7831cf2d547667638f9a16c9c9dbfe89bb5f3
dkodnik <dkodnik@gmail.com>
2023-04-24 15:15:36 +0300

    colab b_v_b ИИ

--- bot_v_bot_2.py
+++ bot_v_bot_2.py
@@ -1,57 +1,20 @@
 import h5py
 
-from dlgo import goboard
-from dlgo import gotypes
-from dlgo import rl
-from dlgo import scoring
 from dlgo.agent.pg import load_policy_agent
+from dlgo.rl.simulate import experience_simulation
 
-BOARD_SIZE = 9
-num_games = 10
-experience_filename = "ex.h5"
+num_games = 1000
+experience_filename = "alphago_rl_experience.h5"
 agent_filename = "./agents/sevenplane.h5"
-
-def simulate_game(black_player, white_player):
-    game = goboard.GameState.new_game(BOARD_SIZE)
-    agents = {
-        gotypes.Player.black: black_player,
-        gotypes.Player.white: white_player,
-    }
-    while not game.is_over():
-        next_move = agents[game.next_player].select_move(game)
-        game = game.apply_move(next_move)
-    game_result = scoring.compute_game_result(game)
-    return game_result.winner
 
 def main():
     agent1 = load_policy_agent(h5py.File(agent_filename))
     agent2 = load_policy_agent(h5py.File(agent_filename))
-    collector1 = rl.ExperienceCollector()
-    collector2 = rl.ExperienceCollector()
-    agent1.set_collector(collector1)
-    agent2.set_collector(collector2)
 
-    for i in range(num_games):
-        collector1.begin_episode()
-        collector2.begin_episode()
+    experience = experience_simulation(num_games, agent1, agent2)
 
-        game_record = simulate_game(agent1, agent2)
-        if game_record.winner == gotypes.Player.black:
-            #агент agent1 победил, поэтому он получает положительную награду
-            collector1.complete_episode(reward=1)
-            collector2.complete_episode(reward=-1)
-        else:
-            # агент agent2 победил
-            collector1.complete_episode(reward=-1)
-            collector2.complete_episode(reward=1)
-
-        #сохранение пакета данных опыта, со объединением данных опыта обоих агентов в единый буфер
-        experience = rl.load_experience([
-            collector1,
-            collector2])
-        with h5py.File(experience_filename, 'w') as experience_outf: #сохранение результата в файд HDF5
-            experience.serialize(experience_outf)
-
+    with h5py.File(experience_filename, 'w') as experience_outf: #сохранение результата в файд HDF5
+        experience.serialize(experience_outf)
 
 if __name__ == '__main__':
     main()
---
+++ dlgo/rl/simulate.py
@@ -0,0 +1,55 @@
+from dlgo import goboard
+from dlgo import gotypes
+from dlgo import rl
+from dlgo import scoring
+
+#from collections import namedtuple
+#class GameRecord(namedtuple('GameRecord', 'moves winner margin')):
+#    pass
+
+def simulate_game(black_player, white_player):
+    board_size = 19
+    #moves = []
+    game = goboard.GameState.new_game(board_size)
+    agents = {
+        gotypes.Player.black: black_player,
+        gotypes.Player.white: white_player,
+    }
+    while not game.is_over():
+        next_move = agents[game.next_player].select_move(game)
+        game = game.apply_move(next_move)
+        #moves.append(next_move)
+    game_result = scoring.compute_game_result(game)
+    print("Победил:", game_result.winner)
+    return game_result
+    #return GameRecord(
+    #    moves=moves,
+    #    winner=game_result.winner,
+    #    margin=game_result.winning_margin,
+    #)
+
+def experience_simulation(num_games, agent1, agent2):
+    collector1 = rl.ExperienceCollector()
+    collector2 = rl.ExperienceCollector()
+    agent1.set_collector(collector1)
+    agent2.set_collector(collector2)
+
+    for i in range(num_games):
+        collector1.begin_episode()
+        collector2.begin_episode()
+
+        game_record = simulate_game(agent1, agent2)
+        if game_record.winner == gotypes.Player.black:
+            # агент agent1 победил, поэтому он получает положительную награду
+            collector1.complete_episode(reward=1)
+            collector2.complete_episode(reward=-1)
+        else:
+            # агент agent2 победил
+            collector1.complete_episode(reward=-1)
+            collector2.complete_episode(reward=1)
+
+    # сохранение пакета данных опыта, со объединением данных опыта обоих агентов в единый буфер
+    experience = rl.load_experience([
+        collector1,
+        collector2])
+    return experience
