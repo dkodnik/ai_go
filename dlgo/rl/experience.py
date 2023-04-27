9f005f753439112a15fa594bdc244e948fa65df8
dkodnik <dkodnik@gmail.com>
2023-04-27 12:39:35 +0300

    добавлен параметр - 'преимущество'

--- dlgo/rl/experience.py
+++ dlgo/rl/experience.py
@@ -2,10 +2,11 @@ import numpy as np
 
 class ExperienceBuffer:
     """Минимальный контейнер для хранения набора данных опыта"""
-    def __init__(self, states, actions, rewards):
+    def __init__(self, states, actions, rewards, advantages):
         self.states = states
         self.actions = actions
         self.rewards = rewards
+        self.advantages = advantages #преимущество (с гл.12)
 
     def serialize(self, h5file):
         """Сохранение буфера опыта на диск"""
@@ -16,28 +17,36 @@ class ExperienceBuffer:
             'actions', data=self.actions)
         h5file['experience'].create_dataset(
             'rewards', data=self.rewards)
+        h5file['experience'].create_dataset(
+            'advantages', data=self.advantages)
 
 class ExperienceCollector:
     """Объект для отслеживания решений, принятых в ходе одного эпизода"""
 
     def __init__(self):
+        # Могут охватывать множество эпизодов
         self.states = []
         self.actions = []
         self.rewards = []
+        self.advantages = [] #преимущество (с гл.12)
+
+        #Сбрасываются в конце каждого эпизода
         self.current_episode_states = []
         self.current_episode_actions = []
+        self.current_episode_estimated_values = [] #(с гл.12)
 
     def begin_episode(self):
         """Вызывается драйвером игры бота с самим собой для обозначения начала отдельной партии"""
         self.current_episode_states = []
         self.current_episode_actions = []
 
-    def record_decision(self, state, action):
+    def record_decision(self, state, action, estimated_value=0):
         """Вызывается агентом для обозначения одного выбранного им действия"""
         #сохранение одного решения в текущем эпизоде; за кодирование состояния и действия
         #отвечает агент
         self.current_episode_states.append(state)
         self.current_episode_actions.append(action)
+        self.current_episode_estimated_values.append(estimated_value) #принятие оценочного значения (с гл.12)
 
     def complete_episode(self, reward):
         """Вызывается драйвером игры бота с самим собой для обозначения окончания отдельной партии"""
@@ -47,8 +56,16 @@ class ExperienceCollector:
         #распределение итоговой награды среди всех действий, совершенных в ходе партии
         self.rewards += [reward for _ in range(num_states)]
 
+        #вычисление преимущества каждого решения (с гл.12)
+        for i in range(num_states):
+            advatage = reward - \
+                self.current_episode_estimated_values[i]
+            self.advantages.append(advatage)
+
+        #сброс буферов для эпизодов
         self.current_episode_states = []
         self.current_episode_actions = []
+        self.current_episode_estimated_values = []
 
     def to_buffer(self):
         """Вызывается по завершению партии. Упаковываются все данные, собранные
@@ -64,13 +81,13 @@ def combine_experience(collectors):
     combined_states = np.concatenate([np.array(c.states) for c in collectors])
     combined_actions = np.concatenate([np.array(c.actions) for c in collectors])
     combined_rewards = np.concatenate([np.array(c.rewards) for c in collectors])
-    #combined_advantages = np.concatenate([np.array(c.advantages) for c in collectors])
+    combined_advantages = np.concatenate([np.array(c.advantages) for c in collectors])
 
     return ExperienceBuffer(
         combined_states,
         combined_actions,
         combined_rewards,
-        #combined_advantages,
+        combined_advantages,
     )
 
 def load_experience(h5file):
